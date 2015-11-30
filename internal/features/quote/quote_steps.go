4d18359d0ed47b5fe9524080ae55e65253314855
d-smith <doug.smith.mail@gmail.com>
2015-11-30 09:33:23 -0800

    Added mb image link

--- internal/features/quote/quote_steps.go
+++ internal/features/quote/quote_steps.go
@@ -1,6 +1,11 @@
 package quote
 
-import . "github.com/lsegal/gucumber"
+import (
+	. "github.com/lsegal/gucumber"
+	"net/http"
+	"fmt"
+	"io/ioutil"
+)
 
 func init() {
 	Given(`^a symbbol$`, func() {
@@ -13,6 +18,20 @@ func init() {
 
 	Then(`^the current price is returned$`, func() {
 		//T.Errorf("fail!")
+		resp, err := http.Get("http://mb:2525/imposters")
+		if err != nil {
+			fmt.Print(err.Error())
+			return
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Print(err.Error())
+			return
+		}
+
+		fmt.Println("Read ->", string(body))
+		resp.Body.Close()
 	})
 
 }
